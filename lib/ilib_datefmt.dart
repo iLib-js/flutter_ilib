d658ca86cbd51e60fd8d0625240ad1d9e0236554
Goun Lee <goun.lee@lge.com>
2024-06-05 16:58:31 +0900

    wip. trying to implement it differently

---
+++ lib/ilib_datefmt.dart
@@ -0,0 +1,86 @@
+import 
+class DateFormat {
+  DateFormatOptions? options;
+
+  DateFormat(){
+    // constructor
+  }
+
+  String? format(DateTime date) {
+    String result;
+    print("format");
+
+    String jscode1 = 'new DateFmt(${this.options.toJsonString}).format(${date.getTime()})';
+    return flutterIlibPlugin.evaluateILib(jscode1);
+  }
+
+}
+
+class DateFormatOptions {
+  String? locale;
+  String? type;
+  String? length;
+  String? timezone;
+  bool? useNative;
+
+  DateFormatOptions(
+    {
+      this.locale,
+      this.length,
+      this.type,
+      this.timezone = 'local',
+      this.useNative
+    } 
+  );
+
+  toJsonString() => '{locale: "$locale", length: "$length", useNative: $useNative, type: "$type", timezone: "$timezone"}';
+}
+
+class DateOptions {
+  String? locale;
+  String? year;
+  String? month;
+  String? day;
+  String? hour;
+  String? minute;
+  String? second;
+  String? unixtime;
+  String? timezone;
+  String? type;
+  DateTime? dateTime;
+
+  DateOptions(
+    {
+      this.locale,
+      this.year,
+      this.month,
+      this.day,
+      this.hour,
+      this.minute,
+      this.second,
+      this.unixtime,
+      this.timezone,
+      this.dateTime,
+      this.type
+    }
+  );
+
+  String toJsonString() {
+    String y = '$year';
+    String m = '$month';
+    String d = '$day';
+    String h = '$hour';
+    String min = '$minute';
+    String sec = '$second';
+
+    if (dateTime != null) {
+      y = '${dateTime!.year}';
+      m = '${dateTime!.month}';
+      d = '${dateTime!.day}';
+      h = '${dateTime!.hour}';
+      min = '${dateTime!.minute}';
+      sec = '${dateTime!.second}';
+    }
+    return '{year:$y, month:$m, day:$d, hour:$h, minute:$min, second:$sec}';
+  }
+}
---
+++ lib/init_ilib.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_js/flutter_js.dart';
+
+
+final JavascriptRuntime _jsRuntime = getJavascriptRuntime();
+final Future<String> _ilib =
+    rootBundle.loadString("packages/flutter_ilib/assets/js/ilib-standard-flutter-compiled.js");
+bool _iLibPrepared = false;
+
+Future<JavascriptRuntime?> initializeiLib([String? locale, String? ignored]) async {
+  
+  try {    
+      final jsiLibFile = await _ilib;
+      _jsRuntime.evaluate(jsiLibFile);
+      _iLibPrepared = true;
+      return _jsRuntime;
+
+    } on PlatformException catch (e) {
+      print('Failed to init js engine: ${e.details}');
+    }
+}
